db0580652120862c715d3de715899bcc7b836da7
Bill McDowell <forkunited@gmail.com>
2017-03-17 00:28:41 -0700

    Constructed new train/dev/test split where (1) rounds from a single game are shuffled across train/dev/test, (2) contexts in training do not occur in dev or test, and (3) if a target object occurs in test or dev then it occurs in training

---
+++ src/js/util.js
@@ -0,0 +1,43 @@
+var getUtteranceActionObjectTarget = function(uA) {
+    var target = "";
+    if (parseInt(uA.action.lTarget_0) == 1)
+        target = uA.action.lName_0;
+    else if (parseInt(uA.action.lTarget_1) == 1)
+        target = uA.action.lName_1;
+    else if (parseInt(uA.action.lTarget_2) == 1)
+        target = uA.action.lName_2;
+    return target;
+};
+
+var getUtteranceActionObjectContext = function(uA) {
+    var target = getUtteranceActionObjectTarget(uA);
+    var o0 = uA.action.lName_0;
+    var o1 = uA.action.lName_1;
+    var o2 = uA.action.lName_2;
+
+    var objs = [o0,o1,o2];
+    objs.sort();
+    return objs[0] + "." + objs[1] + "." + objs[2] + "-" + target
+};
+
+var contextToRoundPartition = function(part) {
+    var keyFn = function(key, uA) {
+        var gameid = gameppl.rgame.getUtteranceActionPairGame(uA);
+        var roundNum = gameppl.rgame.getUtteranceActionPairRound(uA)
+        return gameid + "_" + roundNum;
+    };
+
+    var valueFn = function(key, uA) {
+        var gameid = gameppl.rgame.getUtteranceActionPairGame(uA);
+        var roundNum = gameppl.rgame.getUtteranceActionPairRound(uA)
+        return { "gameid" : gameid, "roundNum" : roundNum };
+    };
+
+    gameppl.partition.mapKeysAndValues(part, keyFn, valueFn);
+}
+
+module.exports = {
+    getUtteranceActionObjectTarget : getUtteranceActionObjectTarget,
+    getUtteranceActionObjectContext : getUtteranceActionObjectContext,
+    contextToRoundPartition : contextToRoundPartition
+};
